2f46eb2301a4934a9d9e0d5d4ab6a7d86ffcfea2
johnahumanbeing <mseewak@gmail.com>
2024-10-02 22:26:34 +0300

    decrementing in js

---
+++ decrementing.js
@@ -0,0 +1,5 @@
+// decrementing means decreasing the value of a variable by 1
+var myVar = 87;
+myVar--; // myVar is now equal to 86
+// you can also decrement by more than 1
+myVar -= 2; // myVar is now equal to 84
