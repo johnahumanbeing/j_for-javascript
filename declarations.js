23672b7863bf1659d625c82c1865aa11fdc45f5d
johnahumanbeing <mseewak@gmail.com>
2024-10-02 22:01:34 +0300

    declarations in js

---
+++ declarations.js
@@ -0,0 +1,9 @@
+// Declarations in javascript are case sensitive
+var StUdLyCapVar;
+var properCamelCase;
+var TitleCaseOver;
+
+// Assignments
+STUDLYCAPVAR = 10;
+PRoperCAmelCAse = "A String";
+tITLEcASEoVER = 9000;
