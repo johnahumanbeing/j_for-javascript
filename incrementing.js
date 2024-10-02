9b659ca88f6b8cf94a83bb1d53fadfa8ff87b0e6
johnahumanbeing <mseewak@gmail.com>
2024-10-02 22:14:46 +0300

    incrementing in js

---
+++ incrementing.js
@@ -0,0 +1,3 @@
+// To  increment a number means to add 1 to it. The increment operator is a shortcut to adding 1 to a variable.
+var myVar = 87;
+myVar = myVar + 1; // or myVar++; which is the same as myVar = myVar + 1;
