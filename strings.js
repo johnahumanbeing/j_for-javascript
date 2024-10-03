f9ba6ec557fd2ede1e86509036d4a37426258f68
johnahumanbeing <mseewak@gmail.com>
2024-10-03 22:49:30 +0300

    strings in js

---
+++ strings.js
@@ -0,0 +1,10 @@
+// strings are used to store the text that is displayed on the screen
+// strings are enclosed in quotes
+// strings can be concatenated with the + operator
+// strings can be concatenated with variables
+// strings can be concatenated with numbers
+
+var firstName = "John";
+var lastName = "Wayne";
+var fullName = firstName + " " + lastName;
+console.log(fullName);
