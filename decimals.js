0cb87015c5449d317070e9560e044d90ceba9984
johnahumanbeing <mseewak@gmail.com>
2024-10-03 22:10:22 +0300

    decimals in js

---
+++ decimals.js
@@ -0,0 +1,12 @@
+// decimals are also known as floating point numbers or floats.
+var myDecimal = 5.7;
+// myDecimal is a decimal number
+var ourDecimal = 2.007;
+
+// you can also multiply decimals
+var product = myDecimal * ourDecimal; // product is equal to 0.0399
+console.log(product);
+
+// you can also divide decimals
+var quotient = myDecimal / ourDecimal; // quotient is equal to 2.837
+console.log(quotient);
