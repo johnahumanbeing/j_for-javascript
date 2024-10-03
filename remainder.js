290b1ccbb2c5e8d504bb317e3e1f19821247b76c
johnahumanbeing <mseewak@gmail.com>
2024-10-03 22:14:41 +0300

    remainder in js

---
+++ remainder.js
@@ -0,0 +1,6 @@
+// The remainder operator looks like this: (%) a percent sign. It returns the remainder of dividing two numbers.
+
+var remainder;
+remainder = 11 % 3; // remainder is equal to 2
+console.log(remainder);
+// sometimes remainder is used to check if a number is even or odd.
