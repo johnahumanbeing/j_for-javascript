1b3a3ec35a6c19d59b689680017c26390c41b394
johnahumanbeing <mseewak@gmail.com>
2024-10-09 22:21:54 +0300

    bracket notation

---
+++ strings/bracket-notation.js
@@ -0,0 +1,15 @@
+// bracket notation is a way to get a character at a specific index within a srting
+
+var firstLetterOfFirstName = "";
+var firstName = "Tottenham";
+
+firstLetterOfFirstName = firstName[0];
+// [0] will return the first character of the string
+
+var firstLetterOfLastName = "";
+var lastName = "Hotspurs";
+
+firstLetterOfLastName = lastName[0];
+
+console.log(firstLetterOfFirstName);
+console.log(firstLetterOfLastName);
