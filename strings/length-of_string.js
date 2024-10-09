82d39d74642a57d501cd56985c60a747e295962f
johnahumanbeing <mseewak@gmail.com>
2024-10-09 22:00:50 +0300

    checking the length of a srting

---
+++ strings/length-of_string.js
@@ -0,0 +1,14 @@
+var firstNameLength = 0;
+var firstName = "Tottenham";
+
+firstNameLength = firstName.length;
+// .length will return the length of the string
+
+//setup
+var lastNameLength = 0;
+var lastName = "Hotspurs";
+
+lastNameLength = lastName.length;
+
+console.log(firstNameLength);
+console.log(lastNameLength);
