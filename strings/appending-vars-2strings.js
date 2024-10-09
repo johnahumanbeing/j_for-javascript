ab8026c58e48c0a8e67eb714d121f7a10bdb6a50
johnahumanbeing <mseewak@gmail.com>
2024-10-09 21:49:28 +0300

    appending variables to strings

---
+++ strings/appending-vars-2strings.js
@@ -0,0 +1,6 @@
+// you can append variables to strings using += operator
+var name = "John";
+var myCountry = "Kenya";
+var myCity = "Nairobi.";
+var anOutput = "Hello, my name is " + name + " and I am from " + myCountry + " in the city of " + myCity;
+console.log(anOutput);
