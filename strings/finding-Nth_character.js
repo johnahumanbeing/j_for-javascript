392e74a6b190cff9a2c8d855a9e55302ae9c8729
johnahumanbeing <mseewak@gmail.com>
2024-10-10 01:46:51 +0300

    finding nth character

---
+++ strings/finding-Nth_character.js
@@ -0,0 +1,11 @@
+// you can use bracket notation to get any character position in a string
+
+var firstName = "Tottenham";
+var secondLetterOfFirstName = firstName[1];
+
+var lastName = "Hotspurs";
+
+var thirdLetterOfLastName = lastName[2];
+
+console.log(secondLetterOfFirstName);
+console.log(thirdLetterOfLastName);
