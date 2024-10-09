b7244e0f90ba0c48375f253cba2ad545706c41e4
johnahumanbeing <mseewak@gmail.com>
2024-10-09 21:33:38 +0300

    concatenating strings with the += sign

---
+++ concatenating-strings-with+=.js
@@ -0,0 +1,7 @@
+var aString = "Dissolve parliament. !!! ";
+aString += "They are all thieves.";
+console.log(aString);
+
+var anExample = "Ruto must go. !!! ";
+anExample += "He is a thief.";
+console.log(anExample);
