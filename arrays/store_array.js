624e4519f4a0a1795f987aa858c2ef792a1c3f71
johnahumanbeing <mseewak@gmail.com>
2024-10-11 22:28:43 +0300

    storing in arrays

---
+++ arrays/store_array.js
@@ -0,0 +1,15 @@
+// You can store multiple values in an array.
+// You can access an array element by referring to the index number.
+// Array indexes start at 0.
+// You can change an array element by assigning a new value to the element.
+// You can access the array length by using the length property.
+// You can use the length property to append new values to an array.
+
+// Example
+
+var ourArray = ["John", 23];
+
+var myArray = ["Quincy", 1];
+
+console.log(ourArray);
+console.log(myArray);
