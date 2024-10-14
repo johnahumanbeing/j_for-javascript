6d745a8074481c125597c342675b11c4698e4833
johnahumanbeing <mseewak@gmail.com>
2024-10-14 19:30:35 +0300

    push method

---
+++ arrays/push().js
@@ -0,0 +1,8 @@
+// you can manipulate Arrays with push () method
+// push () method adds one or more elements to the end of an array and
+// returns the new length of the array.
+
+// Example
+var arr = [1, 2, 3];
+arr.push(4);
+console.log(arr);
