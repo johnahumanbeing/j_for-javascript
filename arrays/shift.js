20d0ad7378e88356e15bd387177fb76bcb23029c
johnahumanbeing <mseewak@gmail.com>
2024-10-14 19:42:27 +0300

    shift method

---
+++ arrays/shift.js
@@ -0,0 +1,7 @@
+// you can use the shift method to remove the first item from an array
+// and return it. This method changes the length of the array.
+
+const arr = [1, 2, 3, 4, 5];
+const firstItem = arr.shift();
+
+console.log(firstItem);
