b835ce79c5465e535b6a17ffe7b933687ff96a46
johnahumanbeing <mseewak@gmail.com>
2024-10-14 20:05:11 +0300

    reusable functions

---
+++ functions/reusable.js
@@ -0,0 +1,9 @@
+// you can have reusable functions here
+function ourReusableFunction()
+{
+  console.log("Ruto Must Go !!");
+}
+
+ourReusableFunction();
+// if you call the function here again, it will print the same message again.
+ourReusableFunction();
