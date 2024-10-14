84685483091f78afd7d92038d2294e35af796fb6
johnahumanbeing <mseewak@gmail.com>
2024-10-14 19:33:38 +0300

    pop method

---
+++ arrays/pop.js
@@ -0,0 +1,7 @@
+// youu can use the pop() method to remove the last element from an array.
+// pop() method removes the last element from an array and returns that element.
+
+// Example
+var arr = [1, 2, 3];
+arr.pop();
+console.log(arr);
