a7689e224907172f3f0797fe737bded961ca3682
johnahumanbeing <mseewak@gmail.com>
2024-10-15 20:44:30 +0300

    global scope

---
+++ functions/global-scope.js
@@ -0,0 +1,26 @@
+var myGlobal = 10;
+
+function func1()
+{
+  oopsGlobal = 5;
+}
+
+function func2()
+{
+  var output = "";
+
+  if (typeof myGlobal != "undefined")
+  {
+    output += "myGlobal: " + myGlobal;
+  }
+
+  if (typeof oopsGlobal != "undefined")
+  {
+    output += " oopsGlobal: " + oopsGlobal;
+  }
+
+  console.log(output);
+}
+
+func1();
+func2();
