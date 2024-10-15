7184e93a8cd850e462719203392c38877b7e4480
johnahumanbeing <mseewak@gmail.com>
2024-10-15 20:29:51 +0300

    arguments

---
+++ functions/arguments.js
@@ -0,0 +1,9 @@
+// arguments are the values that are passed to a
+// function when it is called
+
+function aFunctionWithArguments(a, b)
+{
+  console.log(a + b);
+}
+
+aFunctionWithArguments(1, 2);
