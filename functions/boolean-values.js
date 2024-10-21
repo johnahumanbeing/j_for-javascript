871b51cf46361b76446cc57d6dcb3b68bcf07eb2
johnahumanbeing <mseewak@gmail.com>
2024-10-21 20:52:25 +0300

    boolean values, true or false

---
+++ functions/boolean-values.js
@@ -0,0 +1,12 @@
+// boolean values are true or false values 
+// they are used to make decisions in code
+// they are used in conditional statements
+
+// example of boolean values
+
+function RutoIsGood() // ruto is baad therefore this function should return false
+{
+    return false;
+}
+
+console.log(RutoIsGood());
