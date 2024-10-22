a127eb2aa676f194b5feeb21385b9f0445e084c2
johnahumanbeing <mseewak@gmail.com>
2024-10-22 21:17:34 +0300

    equality operators

---
+++ operators/equality-operators.js
@@ -0,0 +1,13 @@
+// equality operators are used to compare two values and return a boolean value
+// == is the equality operator
+
+function isEqual(val)
+{
+    if (val == 15)
+    {
+        return "Equal";
+    }
+    return "Not Equal";
+}
+
+console.log(isEqual(10));
