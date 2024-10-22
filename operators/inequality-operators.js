2d36122dda37ba456e52a9312fbd4b191482e889
johnahumanbeing <mseewak@gmail.com>
2024-10-22 21:17:58 +0300

    inequality operators

---
+++ operators/inequality-operators.js
@@ -0,0 +1,12 @@
+// inequality operators are used to compare two values and return a boolean value
+// they look like this: !=, !==, <, >, <=, >=
+
+let a = 10;
+let b = 20;
+
+console.log(a != b);  // true
+console.log(a !== b); // true
+console.log(a < b);   // true
+console.log(a > b);   // false
+console.log(a <= b);  // true
+console.log(a >= b);  // false
