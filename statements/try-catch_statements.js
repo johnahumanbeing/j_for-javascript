ef49cbd556981925b9c1decd77afdbb2da5832ad
johnahumanbeing <mseewak@gmail.com>
2024-10-23 21:57:47 +0300

    try catch statements

---
+++ statements/try-catch_statements.js
@@ -0,0 +1,8 @@
+// try-catch is used for handling errors in JavaScript.
+
+try {
+    let result = 5 / 0;
+    console.log(result);
+  } catch (error) {
+    console.log("An error occurred: " + error);
+  }
