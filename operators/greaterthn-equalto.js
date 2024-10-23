2076cdf3c4f29d27288a99105174b0c237c13ea3
johnahumanbeing <mseewak@gmail.com>
2024-10-23 21:11:13 +0300

    greater than or equal to operator

---
+++ operators/greaterthn-equalto.js
@@ -0,0 +1,9 @@
+// the greater than or equal to (>=) operator in JavaScript
+
+let a = 10;
+let b = 5;
+let c = 10;
+
+console.log(a >= b); // true, because 10 is greater than 5
+console.log(a >= c); // true, because 10 is equal to 10
+console.log(b >= c); // false, because 5 is not greater than or equal to 10
