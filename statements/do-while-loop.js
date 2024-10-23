6cf7bce92709ec99ca631afbe37e72deb94ba795
johnahumanbeing <mseewak@gmail.com>
2024-10-23 21:53:53 +0300

    do while loop

---
+++ statements/do-while-loop.js
@@ -0,0 +1,9 @@
+// A do-while loop is similar to the while loop but ensures the
+// block of code runs at least once before checking the condition.
+
+let number = 0;
+
+do {
+  console.log("Number is: " + number);
+  number++;
+} while (number < 3);
