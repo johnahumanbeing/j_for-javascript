752df7c738e30a505f8c4a9889ac105c98eb7606
johnahumanbeing <mseewak@gmail.com>
2024-10-23 21:37:40 +0300

    else statements

---
+++ statements/else-statements.js
@@ -0,0 +1,21 @@
+let temperature = 30;
+
+if (temperature > 25)
+    {
+    console.log("It's hot outside!");
+    }
+else
+{
+    console.log("It's not that hot outside.");
+}
+
+let isRaining = false;
+
+if (isRaining)
+    {
+    console.log("Don't forget your umbrella!");
+    }
+ else 
+{
+    console.log("No need for an umbrella today.");
+}
