3d5d6c12790e9d6818c6c86087a388655585eda7
johnahumanbeing <mseewak@gmail.com>
2024-10-23 21:13:11 +0300

    less than or equal to operator

---
+++ operators/lessthn-equalto.js
@@ -0,0 +1,9 @@
+// the less than or equal to (<=) operator in JavaScript
+
+let a = 10;
+let b = 20;
+let c = 10;
+
+console.log(a <= b); // true, because 10 is less than 20
+console.log(a <= c); // true, because 10 is equal to 10
+console.log(b <= a); // false, because 20 is not less than or equal to 10
