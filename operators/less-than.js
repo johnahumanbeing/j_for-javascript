342879ac0a43c975096dd058115b3b0fe5c4b8fb
johnahumanbeing <mseewak@gmail.com>
2024-10-23 21:08:57 +0300

    less than operator

---
+++ operators/less-than.js
@@ -0,0 +1,12 @@
+// the less than (<) operator in JavaScript
+
+let a = 5;
+let b = 10;
+
+if (a < b) {
+    console.log(`${a} is less than ${b}`);
+} else {
+    console.log(`${a} is not less than ${b}`);
+}
+
+// Output: 5 is less than 10
