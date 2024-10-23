ee7638b0043747f2cc99239969e336d17c5a92c9
johnahumanbeing <mseewak@gmail.com>
2024-10-23 21:07:19 +0300

    greater than opertor

---
+++ operators/greater-than.js
@@ -0,0 +1,12 @@
+// Example of using the greater than (>) operator in JavaScript
+
+let a = 10;
+let b = 5;
+
+if (a > b) {
+    console.log(`${a} is greater than ${b}`);
+} else {
+    console.log(`${a} is not greater than ${b}`);
+}
+
+// Output: 10 is greater than 5
