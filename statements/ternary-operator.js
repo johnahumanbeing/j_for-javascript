ea51cb408107e80af329f49393b4b7c7eeaebe64
johnahumanbeing <mseewak@gmail.com>
2024-10-23 21:55:44 +0300

    ternary operator

---
+++ statements/ternary-operator.js
@@ -0,0 +1,6 @@
+// A  is a shorthand for if-else 
+// and is useful for simple conditional expressions.
+
+let age = 20;
+let message = (age >= 18) ? "You are an adult." : "You are a minor.";
+console.log(message);
